ef5bc17807e73f95556be2a138415b99c1c0f105
Tihi321 <knox@dr.com>
2018-05-17 17:38:41 +0200

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDZTuxDxEcsFmy6Lcs5UMnsIcye5P9WaGE",
-  authDomain: "todo-list-9aa22.firebaseapp.com",
-  databaseURL: "https://todo-list-9aa22.firebaseio.com",
-  projectId: "todo-list-9aa22",
-  storageBucket: "todo-list-9aa22.appspot.com",
-  messagingSenderId: "1047837366354"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 const fire = firebase.initializeApp(config);
